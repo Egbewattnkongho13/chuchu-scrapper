253aa6d3b2bfa37dfcde5118fa19ad9806a8d994
Egbewatt Nkongho <egbewattnkonghob@gmail.com>
2025-04-22 10:06:38 +0100

    Create sample html  file for mock testing

---
+++ tests/test_chuchu_scraper.py
@@ -0,0 +1,9 @@
+"""Tests for the ChuChuScrapper class."""
+# import os
+# import pytest
+# from unittest.mock import patch, MagicMock
+# import requests_mock
+# from pathlib import Path
+
+# #Import and test the ChuChuScrapper class and its methods.
+# from chuchu_scrapper.scrapper import ChuChuScrapper
